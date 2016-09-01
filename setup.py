fd3f20032716df6d809eb77accb2347fd10ab7e8
Mark Longair <mhl@pobox.com>
2016-09-01 10:40:09 +0100

    Bump the package version to 0.0.3 for a new release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "everypolitician",
-    version = "0.0.2",
+    version = "0.0.3",
     packages = find_packages(),
     author = "Mark Longair",
     author_email = "mark@mysociety.org",
