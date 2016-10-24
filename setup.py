ef3dd150b7160b66119c495b8b28f94f270ec5a6
Mark Longair <mhl@pobox.com>
2016-10-24 09:24:18 +0100

    Version 0.0.9, with the updated everypolitician-popolo-python

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "everypolitician",
-    version = "0.0.8",
+    version = "0.0.9",
     packages = find_packages(),
     author = "Mark Longair",
     author_email = "mark@mysociety.org",
